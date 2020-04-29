f92b1682b599bd7fc59309305d1380bf5e7c409e
Edgar Romero <edgaromero1987@gmail.com>
2020-04-28 21:44:40 -0700

    create a web page abouut and questions with js

---
+++ js/app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+alert('Greetings Friends');
+
+//first question to know visitors name
+var userName = prompt('What is yor name?');
+console.log('Prompt for username. User answered: ' + userName );
+
+//second question if username wants try a game
+var canYouGuess = prompt('Hola ' + userName + ' do you wanna guess where I am from?');
+if(canYouGuess.toLowerCase() === 'yes'){
+  alert('great, I will help you with ease questions.');
+} else if(canYouGuess.toLowerCase() === 'no'){
+  alert('Too bad, you have to do it');}
+prompt('Please answer with a yes or a no');
+
+//third question if username know about tequila
+var drinkedTequila = prompt('Have you ever try: Tequila?');
+if(drinkedTequila.toLowerCase() === 'yes'){
+  alert('I hope it was just a taste hehe.');
+} else if(drinkedTequila.toLowerCase() === 'no'){
+  alert('You should give it a try.');
+}
+//fourth question about avocados
+var avocados = prompt('Do you like Avocados?');
+if(avocados.toLowerCase()=== 'yes'){
+  alert('Good, where I am from produces most of it.');
+} else if(avocados.toLowerCase() === 'no'){
+  alert('No guacamole for you my friend.');
+}
+//fifth question about taco tuesdays
+var tacos = prompt('Do you know what is great on Tuesdays?');
+if(tacos.toLowerCase() === 'yes'){
+  alert('Oh yes, Tacos!');
+} else if(tacos.toLowerCase() === 'no'){
+  alert('That is unacceptable');
+}
+
+alert('Well ' + userName + ' I think is pretty clear now, I am from Italy');
+alert('Adios My friend');
+
+
+console.log(userName + ' guess where from ' + canYouGuess);
